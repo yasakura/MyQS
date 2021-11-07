e5d1439c7a64861f2e0dbd451aa19e0eff9403f0
Lionel JAMAL <freelance@ljamal.net>
2021-11-07 22:17:38 +0100

    First stable version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { PieChart } from 'react-minimal-pie-chart';
 
 function App() {
+    // const chart = PieChart(population, {
+    //     name: d => d.name,
+    //     value: d => d.value,
+    //     width,
+    //     height: 500
+    // })
+  const handleSubmit = async (event) => {
+      event.preventDefault()
+      const date = event.target.date.value;
+      const diet = event.target.diet.value;
+      const savedDiet = JSON.parse(localStorage.getItem("diet")) || [];
+      const dietEntry = {date, diet};
+      let dietEntryToSave = [...savedDiet, dietEntry]
+
+      localStorage.setItem("diet", JSON.stringify(dietEntryToSave))
+  }
+
+  const getData = () => {
+      const savedDiet = JSON.parse(localStorage.getItem("diet")) || [];
+      const veganDiet = savedDiet.filter((diet) => diet.diet === "vegan")
+      const vegetarianDiet = savedDiet.filter((diet) => diet.diet === "vegetarian")
+      const omnivoreDiet = savedDiet.filter((diet) => diet.diet === "omnivore")
+
+      return [
+          { title: 'vegan', value: veganDiet.length, color: '#E38627' },
+          { title: 'végétarien', value: vegetarianDiet.length, color: '#C13C37' },
+          { title: 'omnivore', value: omnivoreDiet.length, color: '#6A2135' },
+      ]
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header style={{marginBottom: "20px"}}>My QS</header>
+        <div style={{width: "70%", height: "300px", margin: "0 auto 30px"}}>
+            <PieChart
+                data={getData()}
+                label={({ dataEntry }) => (
+                    dataEntry.value > 0 ? `${dataEntry.title} : ${dataEntry.value}` : null
+                )}
+                labelStyle={{fontSize: "7px"}}
+            />
+        </div>
+
+      <form onSubmit={handleSubmit}>
+          <label>
+              Date{" "}
+              <input type="date" name="date" id="date"/>
+          </label><br/><br/>
+        <label>
+          Végétarien
+
+          <input type="radio" name="diet" value="vegetarian"/>
+        </label><br/><br/>
+          <label>
+              Végan
+            <input type="radio" name="diet" value="vegan"/>
+          </label><br/><br/>
+          <label>
+              Omnivore
+          <input type="radio" name="diet" value="omnivore"/>
+          </label><br/><br/>
+          <button type="submit">Enregistrer</button>
+      </form>
     </div>
   );
 }
