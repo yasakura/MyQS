91c7fe8077d7f1348b8f7f1bea0227f0449b7317
Lionel JAMAL <freelance@ljamal.net>
2021-12-22 07:45:54 +0100

    add eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["react-app", "plugin:react/recommended", "airbnb", "prettier"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "react/function-component-definition": [
+      0,
+      {
+        namedComponents: "function-expression",
+        unnamedComponents: "function-expression",
+      },
+    ],
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+  },
+};
